482f02333c247a8cea8b78aa2866b8cc12337183
Émile Boucher <60103006+bhuzgujj@users.noreply.github.com>
2022-01-09 01:23:22 -0500

    optimising visibility

--- src/main/java/dev/emileboucher/blackjackml/intelligence/DecisionMaker.java
+++ src/main/java/dev/emileboucher/blackjackml/intelligence/DecisionMaker.java
@@ -11,15 +11,14 @@ import java.util.Random;
 public class DecisionMaker {
     public static Random random = new Random();
     public static RequestBuilder reinforcementLearning(Response response, Boolean isExploring) {
-        if (isExploring) {
-            return (random.nextInt(2) == 0) ? new Hold() : new Hit();
-        }
-        var value = AiSingleton.getInstance().getModel().get(response.toString());
-        if (value != null) {
-            if (value > 10) {
-                return new Hold();
-            } else if (value < -10) {
-                return new Hit();
+        if (!isExploring) {
+            var value = AiSingleton.getInstance().getModel().get(response.toString());
+            if (value != null) {
+                if (value > 10) {
+                    return new Hold();
+                } else if (value < -10) {
+                    return new Hit();
+                }
             }
         }
         return (random.nextInt(2) == 0) ? new Hold() : new Hit();
